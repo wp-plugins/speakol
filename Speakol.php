84e46065b3986f97ae4c54d044eea7f6fd728cc4
infralayer <infralayer@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-04 09:48:13 +0000

    tagging version 1.0
    git-svn-id: https://plugins.svn.wordpress.org/speakol/tags/1.0@1152738 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ Speakol.php
@@ -0,0 +1,90 @@
+<?php
+
+/**
+* Plugin Name: Speakol
+* Plugin URI: 
+* Description: Speakol helps you build your own community and drive up engagement around your content.
+* Author: Team Speakol
+* Author URI: http://plugins.speakol.com
+* Version: 1.0 
+*/
+
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+
+require_once(__DIR__ . '/classes/SpeakolSettings.php');
+
+
+class Speakol
+{
+    private $settings;
+    static function getInstance()
+    {
+        static $instance = null;
+        if(null === $instance) {
+            $instance =  new static();
+        }
+
+        return $instance;
+    }
+
+
+    public function init() {
+        $this->settings = SpeakolSettings::getInstance();
+        add_action('init', array($this, 'registerShortcode'));
+        add_filter('the_content', array($this, 'addSpeakol'));
+    }
+
+
+    public function registerShortcode() {
+        add_shortcode('speakol-argbox', array($this, 'shortcodeFunction'));
+    }
+
+    public function shortcodeFunction($attrs) {
+        $settings = $this->settings->getSettings();
+        extract(shortcode_atts(array(
+            'lang' => $settings['lang'],
+            'width' => $settings['box_width'],
+            'title' => $settings['title'],
+            'notitle' => ( $settings['no_title'] === 'on' ) ? 'true' : 'false',
+        ), $attrs));
+
+        $shortcode = "";
+        require_once(__DIR__ . '/views/argumentsbox.php');
+        return $shortcode;
+    }
+
+
+    public function addSpeakol($content) {
+        $settings = $this->settings->getSettings();
+        $displaysIn = SpeakolSettings::$displaysIn;
+        $pageCondiditon = selected($displaysIn['pages'], $settings['displays_in'], false ) && is_page();
+        $postCondiditon = selected($displaysIn['posts'], $settings['displays_in'], false) && is_single();
+        $bothCondiditon = selected($displaysIn['both'], $settings['displays_in'], false) && (is_single() || is_page());
+        if ($settings['app_id'] && (checked(1, $settings['argbox_status'], false)) && ($pageCondiditon || $postCondiditon || $bothCondiditon)) {
+            $shortcode = do_shortcode('[speakol-argbox]');
+            $content  = $content . $shortcode;       
+        }
+        return $content;
+    }
+
+    protected function __construct()
+    {
+        
+    }
+
+
+    private function __clone()
+    {
+        
+    }
+
+
+    private function __wakeup() {
+        
+    }
+}
+
+$speakol = Speakol::getInstance();
+
+$speakol->init();
+
